eabd089daec5ebac6eb19c740c3ca8d24c6a6a55
mohammed-sarhad-ahmed <muhamadsarhad999@gamil.com>
2023-01-30 23:41:36 +0300

    fix bugs

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ let answer;
 let count = 0
 let shower = 0
 let evaluator = 0
+let secondEvaluator = 0
 let plusOj = document.querySelector("#plus")
 let minusOj = document.querySelector("#minus")
 let multiplyOj = document.querySelector("#mutiplay")
@@ -84,15 +85,19 @@ function getOperator(e) {
         count = 0
     }
     else {
+        e.target.style.backgroundColor = "white"
+        e.target.style.color = "orange"
         point.disabled = false
         if (count === 0) {
             ary.push(screen.innerText)
         }
         evaluator++
+        console.log(evaluator)
 
-        if (evaluator === 2) {
+        if (evaluator === 2 || (secondEvaluator === 1 && evaluator === 1)) {
             screen.innerText = Math.round(answer * 100) / 100
             evaluator = 0
+            secondEvaluator = 0
         }
         shower++
         currentOperator = e.target.id
@@ -104,34 +109,43 @@ function getOperator(e) {
 function doEvaluation(num1, num2, currentOperator) {
 
     if (currentOperator === "divide") {
+
         answer = doOperations.divide(num1, num2)
     }
     else if (currentOperator === "minus") {
+
         answer = doOperations.minus(num1, num2)
     }
     else if (currentOperator === "plus") {
+
         answer = doOperations.plus(num1, num2)
     }
     else if (currentOperator === "mutiplay") {
+
         answer = doOperations.multi(num1, num2)
     }
     while (ary.length) {
         ary.pop()
     }
+    secondEvaluator++
     state = false
     disable(state)
     ary.push(Math.round(answer * 100) / 100)
     assign.disabled = false;
+    getBackgroundColor()
 }
 // if you click = it will show you the answer
 assign.addEventListener("click", () => {
+    getBackgroundColor()
     if (answer == Infinity) {
         screen.innerText = "you broke math"
         evaluator = 0
+        secondEvaluator = 0
     }
     else {
         screen.innerText = Math.round(answer * 100) / 100
         evaluator = 0
+        secondEvaluator = 0
     }
 })
 //clear
@@ -146,20 +160,21 @@ function clearer() {
     screen.innerText = 0
     answer = 0
     shower = 0
-    evaluator = 0 
+    evaluator = 0
     assign.disabled = true;
+    getBackgroundColor()
+    secondEvaluator = 0
+
 }
 //disable button
 function disable(state) {
     if (state === true) {
-
         plusOj.disabled = true
         minusOj.disabled = true
         multiplyOj.disabled = true
         divideOJ.disabled = true
     }
     else {
-
         plusOj.disabled = false
         minusOj.disabled = false
         multiplyOj.disabled = false
@@ -170,3 +185,17 @@ let state = true
 disable(state)
 //assignment disabler
 assign.disabled = true;
+//background color
+function getBackgroundColor() {
+    if (multiplyOj.style.backgroundColor === "white" || plusOj.style.backgroundColor === "white"
+        || divideOJ.style.backgroundColor === "white" || minusOj.style.backgroundColor === "white") {
+        multiplyOj.style.backgroundColor = "orange"
+        multiplyOj.style.color = "white"
+        plusOj.style.backgroundColor = "orange"
+        plusOj.style.color = "white"
+        minusOj.style.backgroundColor = "orange"
+        minusOj.style.color = "white"
+        divideOJ.style.backgroundColor = "orange"
+        divideOJ.style.color = "white"
+    }
+}
